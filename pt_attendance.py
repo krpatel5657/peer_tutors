7a4bac985e286a59735f051f1c75d598479a2c59
Krishna Patel <krpatel5657@gmail.com>
2021-02-27 20:39:11 -0600

    Update pt_attendance.py

--- pt_attendance.py
+++ pt_attendance.py
@@ -3,72 +3,21 @@ import xlrd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-math = xlrd.open_workbook("/Users/krishna/Documents/pt_attendance/FNL-AT-AT PT MATH.xlsx")
+math = xlrd.open_workbook("insert_sheet_name.xlsx")
 math_sheets = []
 for x in range(math.nsheets):
     math_sheets.append(math.sheet_by_index(x))
 print("The num of sheeets is ", len(math_sheets))
-tutors = xlrd.open_workbook("/Users/krishna/Documents/pt_attendance/Peer Tutors Roster Sem1 20-21.xlsx")
+tutors = xlrd.open_workbook("insert_sheet_name.xlsx")
 names = []
 for x in range(2, tutors.sheet_by_index(0).nrows):
     names.append(tutors.sheet_by_index(0).row_values(x)[0])
     name = names[x-2].split(",")
     name = name[1] + " " + name[0]
     name = str(name).strip()
-    if name == "Ribhav Bhatia":
-        name="Ribhiv Bhatia"
-    if name == "Aniketh Bhaskar":
-        name = "Aniketh Bhaska"
-    if name == "Seunghee Han":
-        name = "Olivia Han"
-    if name == "Dylan Heydenburg":
-        name = "Dylan Hydensburg"
-    if name == "Sahil Kamat":
-        name = "Sahit Kamat"
-    if name == "Gyeongwon Kim":
-        name = "Gyeongwon (James) Kim"
-    if name == "Seohyun Kim":
-        name = "Bella Kim"
-    if name == "Ha Eun Kwon":
-        name = "Esther Kwon"
-    if name == "Regina Lapitsky":
-        name = "Rehina (Regina) Lapytska"
-    if name == "Haejoon Lim":
-        name = "John Lim"
-    if name == "Jonathan Mann":
-        name = "Jonathan (Yoni) Mann"
-    if name == "Caroline Mazur Sarocka":
-        name = "Caroline Mazur-Sarocka"
-    if name == "Abigail Minin":
-        name = "Abby Minin"
-    if name == "Madeline Mitchell":
-        name = "Maddie Mitchell"
-    if name == "Varsha Mullangi":
-        name = "Varsha Mulangi"
-    if name == "Aditya Nair":
-        name = "Adi Nair"
-    if name == "Sudha Nallacheruvu":
-        name = "Sailaja Nallacheruvu"
-    if name == "Joshua Neela":
-        name = "Josh Neela"
-    if name == "Alexandra Sokolowski":
-        name = "Alexandra Sokolowki"
-    if name == "Swetha Subramanian":
-        name = "Swetha Subramanium"
-    if name == "Samuel Sweet":
-        name = "Sam Sweet"
-    if name == "Nivedha Vasanth":
-        name = "Nivedha Prasanth"
-    if name == "Gabriel Visotsky":
-        name = "Gabe Visotsky"
-    if name == "Maddison Wang":
-        name = "Maddie Wang"
-    if name == "Natalia Waszynska":
-        name = "Natalia Wasynska"
-    if name == "Mark Younan":
-        name = "Mark Younnan"
-    if name == "Youran Zhu":
-        name = "Youran(Bill) Zhu"
+    if name == "wrong name":
+        name="fixed name"
+
     names[x-2] = name.strip()
 
 tutor_attendance = {}
@@ -84,7 +33,7 @@ for n in names:
                 if str(cell.value).strip() == n and x.cell_value(rowx = rowidx, colx = colidx+1)==True:
                     tutor_attendance[n][0]+=1
 
-math = xlrd.open_workbook("/Users/krishna/Documents/pt_attendance/FNL-Attendance_PeerTutor_SCIENCE.xlsx")
+math = xlrd.open_workbook("insert_sheet_name.xlsx")
 math_sheets = []
 for x in range(math.nsheets):
     math_sheets.append(math.sheet_by_index(x))
@@ -100,7 +49,7 @@ for n in names:
                 if str(cell.value).strip() == n and x.cell_value(rowx = rowidx, colx = colidx+1)==True:
                     tutor_attendance[n][0]+=1
 
-math = xlrd.open_workbook("/Users/krishna/Documents/pt_attendance/FNL-AT_PT_SS.xlsx")
+math = xlrd.open_workbook("insert_sheet_name.xlsx")
 math_sheets = []
 for x in range(math.nsheets):
     math_sheets.append(math.sheet_by_index(x))
@@ -117,7 +66,7 @@ for n in names:
                     tutor_attendance[n][0]+=1
 
 
-math = xlrd.open_workbook("/Users/krishna/Documents/pt_attendance/FNL_OLK_CA_PT_AT.xlsx")
+math = xlrd.open_workbook("insert_sheet_name.xlsx")
 math_sheets = []
 for x in range(math.nsheets):
     math_sheets.append(math.sheet_by_index(x))
@@ -134,7 +83,7 @@ for n in names:
                     tutor_attendance[n][0]+=1
 
 
-math = xlrd.open_workbook("/Users/krishna/Documents/pt_attendance/FNL-AT_PT_WL.xlsx")
+math = xlrd.open_workbook("insert_sheet_name.xlsx")
 math_sheets = []
 for x in range(math.nsheets):
     math_sheets.append(math.sheet_by_index(x))
@@ -150,7 +99,7 @@ for n in names:
                 if str(cell.value).strip() == n and x.cell_value(rowx = rowidx, colx = colidx+1)==True:
                     tutor_attendance[n][0]+=1
 
-math = xlrd.open_workbook("/Users/krishna/Documents/pt_attendance/FNL-AT_PT_GS.xlsx")
+math = xlrd.open_workbook("insert_sheet_name.xlsx")
 math_sheets = []
 for x in range(math.nsheets):
     math_sheets.append(math.sheet_by_index(x))
@@ -166,7 +115,7 @@ for n in names:
                 if str(cell.value).strip() == n and x.cell_value(rowx = rowidx, colx = colidx+1)==True:
                     tutor_attendance[n][0]+=1
 
-math = xlrd.open_workbook("/Users/krishna/Documents/pt_attendance/FNL_PIO_CA_PT_AT.xlsx")
+math = xlrd.open_workbook("insert_sheet_name.xlsx")
 math_sheets = []
 for x in range(math.nsheets):
     math_sheets.append(math.sheet_by_index(x))
@@ -182,7 +131,7 @@ for n in names:
                 if str(cell.value).strip() == n and x.cell_value(rowx = rowidx, colx = colidx+1)==True:
                     tutor_attendance[n][0]+=1
 
-math = xlrd.open_workbook("/Users/krishna/Documents/pt_attendance/DW Attendance tracking for teachers.xlsx")
+math = xlrd.open_workbook("insert_sheet_name.xlsx")
 math_sheets = []
 for x in range(math.nsheets):
     math_sheets.append(math.sheet_by_index(x))
@@ -198,7 +147,7 @@ for n in names:
                 if str(cell.value).strip() == n and x.cell_value(rowx = rowidx, colx = colidx+1)==True:
                     tutor_attendance[n][0]+=1
 
-math = xlrd.open_workbook("/Users/krishna/Documents/pt_attendance/FNL-AT_PT_CS.xlsx")
+math = xlrd.open_workbook("insert_sheet_name.xlsx")
 math_sheets = []
 for x in range(math.nsheets):
     math_sheets.append(math.sheet_by_index(x))
